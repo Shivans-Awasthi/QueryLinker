699c5f11e19d37e15f686c37788af7bdf93418f9
Shivans Awasthi <90861628+Shivans-Awasthi@users.noreply.github.com>
2024-04-26 11:33:06 +0530

    Update search_engine_web_app.py

--- search_engine_web_app.py
+++ search_engine_web_app.py
@@ -19,5 +19,5 @@ def result():
 		return render_template("result.html",result=result)
 
 if __name__ == '__main__':
-	app.run(host='127.0.0.1', port=8000, debug=True)
+	app.run(host='0.0.0.0', port=8000, debug=True)
 	
